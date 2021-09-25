536cc7e4f1bcf17346ea81508f7ad6cc2edca0e7
Amal <amal@pop-os.localdomain>
2021-09-25 11:04:08 +0300

    Third Commit

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
     
+from typing import Text
+from flask import Flask, render_template, request, redirect
+import flask
+#from requests import request
+
+import requests
+
+# Bash
+# export FLASK_APP=application.py
+# flask run
+app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+#db = SQLAlchemy(app)
+
+#SERVER_URL = 'http://127.0.0.1:5000/'
+
+
+        
+
+@app.route('/' , methods = ['GET','POST' ])
+def index():
+
+    return render_template("index.html")
+
+if __name__ == 'main':
+    app.run(debug=True)
