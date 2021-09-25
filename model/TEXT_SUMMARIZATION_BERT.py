fe9b0d6f46664ed2e9127b36302b1857cfc661c1
Amal <amal@pop-os.localdomain>
2021-09-25 19:19:35 +0300

    Summary Commit

--- model/TEXT_SUMMARIZATION_BERT.py
+++ model/TEXT_SUMMARIZATION_BERT.py
@@ -1,3 +1,4 @@
+import torch
 from transformers import pipeline
 
 summarizer = pipeline("summarization")
@@ -335,10 +336,17 @@ nor that he would spend the next few weeks being prodded and pinched by his cous
 He couldn’t know that at this very moment, people meeting in secret all over the country were
 holding up their glasses and saying in hushed voices: “To Harry Potter — the boy who lived!”
  """
-def model_foward(article): 
-    summary = '' 
-    for i in range (int(len(article)/1024)):
-        summary+=summarizer(article[1024*(i-1):1024*i], max_length=100, min_length=50, do_sample=False)[0]
-        #print(summary['summary_text'])
+def model_forward(article): 
+    summary =''
+    if (len(article)>1024):
+        for i in range (0,int(len(article)/1024)-1):
+            if (i== int(len(article)/1024)-1):
+              summ =summarizer(article[1024*(i):], max_length=100, min_length=1, do_sample=False)[0]    
+            else:
+              summ =summarizer(article[1024*(i):1024*(i+1)], max_length=100, min_length=1, do_sample=False)[0]
+            summary += ''+summ['summary_text']
+            #print(summary['summary_text'])
+    else:
+        summary =  summarizer(article, max_length=100, min_length=1, do_sample=False)[0]['summary_text']
     return summary
     
--- static/javascripts/connect_model.js
+++ static/javascripts/connect_model.js
@@ -1,1 +1,20 @@
-let searchQuery = summary;
+let searchQuery = summary+'';
+
+window.onload = () => {
+    searchQuery = summary;
+    generateHTML(searchQuery);
+    console.log(searchQuery);
+};
+
+
+function generateHTML(results) {
+    const BodyDiv = document.getElementById('results-body');
+    let HTML = `
+        <div class="item">
+          
+          <p class="item-data"> ${results}</p>
+        </div>
+      `;
+
+    BodyDiv.innerHTML = HTML;
+  }
